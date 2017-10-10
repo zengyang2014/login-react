b16c0bee260802088fffefea137bd2f2e5a012a8
yzeng <yzeng@thoughtworks.com>
2017-10-10 22:06:29 +0800

    delete comments

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,31 +1,11 @@
 pipeline{
     agent { docker 'node:7.10.1' }
     stages {
-//            stage('Build') {
-//                steps {
-//                    echo 'Building..'
-//                    sh 'echo "Hello Jenkins"'
-//                    sh 'ls -lah'
-//                }
-//            }
             stage('Test') {
                 steps {
                     sh 'echo "Fail!"; exit 1'
                 }
             }
-//            stage('Deploy') {
-//                steps {
-//                    echo 'Deploying....'
-//
-//                    retry(3) {
-//                        sh './fake-deploy.sh'
-//                    }
-//
-//                    timeout(time:1, unit:'MINUTES') {
-//                        sh './fake-check.sh'
-//                    }
-//                }
-//            }
         }
     post {
         always {
